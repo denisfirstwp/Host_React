1583eac6b3fe953b4c889e4730d36d12d17df1b8
Denis <denisfirstwp@gmail.com>
2020-09-04 21:57:51 -0300

    Testando Hospedagem 2

--- src/App.js
+++ src/App.js
@@ -5,23 +5,6 @@ import './App.css'
 export default class App extends React.Component {
 
 
-  componentDidMount() {
-    const containerID = document.getElementById('chave');
-    const container = document.querySelector('.container');
-    container.style.height = '500px';
-    container.style.cursor = 'pointer';
-
-    const carretaFuracao = () => {
-      alert('')
-    }
-
-    container.addEventListener('click', () => {
-      container.style.color = '#FFF';
-      container.style.backgroundColor = 'blue';
-    })
-
-
-  }
 
 
 
